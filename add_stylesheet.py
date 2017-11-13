b68d350ec7fe206d1b1092dca46121c32e9bffc7
Shiying99 <Shiying Li>
2017-11-13 20:01:48 +0100

    add_stylesheet.py manually since git merge master doesnt work to recover the file

---
+++ add_stylesheet.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import sys, os
+filename = sys.argv[1]
+stylesheet = sys.argv[2]
+
+data = open(filename).read()
+
+path = "file://"  + os.path.realpath(stylesheet)
+
+s = '<link rel="stylesheet" type="text/css" href="%s"/>' % path
+
+before = '</head>'
+if s in data:
+    print('Already present ' + s)
+else:
+
+    data2 = data.replace(before, s+before)
+    assert s in data2
+
+    with open(filename, 'w') as f:
+        f.write(data2)
