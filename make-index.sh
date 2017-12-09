9a78bc2fda72dba82f8930585cb048924637b63a
Lucy Newman <newmanlucy@uchicago.edu>
2017-12-08 18:13:59 -0600

    error checking

--- make-index.sh
+++ make-index.sh
@@ -6,6 +6,9 @@ printf -v FILES %s "$(./make-json.py $1)"
 
 #echo $FILES
 
-echo $FILES | node build-index.js > index.json
-
-echo "index successfully created"
+if echo $FILES | node build-index.js > index.json
+then
+	echo "index successfully created"
+else
+	echo "index not created"
+fi
