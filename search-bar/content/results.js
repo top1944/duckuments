083b8de507343fe2a23fd739a6bef81af761a643
Lucy Newman <newmanlucy@uchicago.edu>
2017-12-18 20:13:31 -0500

    improved multiple-word search

--- search-bar/content/results.js
+++ search-bar/content/results.js
@@ -28,21 +28,6 @@ function stemWordsOnly(string) {
         .map(x => stemmer(x));
 }
 
-// filter for containing a word whose stemm is the same as the input's stem
-$.expr[':'].Contains = function(a,i,m) {
-    return stemWords($(a).text())
-        .join(' ')
-        .indexOf(stemmer(m[3].toLowerCase()))>=0;
-};
-
-$.expr[':'].ContainsAll = function(a,i,m) {
-    query = stemWords(m[3])
-        .join(' ');
-    text = stemWords($(a).text())
-        .join(' ');
-    return text.indexOf(query)>=0;
-}
-
 $.expr[':'].ContainsAny = function(a,i,m) {
     query = stemWordsOnly(m[3]);
     text = stemWordsOnly($(a).text())
@@ -76,16 +61,9 @@ function getPageInfo(url, query) {
             var pageDiv = $.grep($(obj), 
                 function(e) {return e.id=="not-toc";})[0];
             var pageTitle = $('h1', pageDiv).text();
-            // var sample = getSample($(pageDiv), query, "ContainsAll");
-            // if (sample == "...") {
-            //     sample = getSample($(pageDiv), query, "ContainsAny");
-            // }
+
             var sample = getFullSample($(pageDiv), query);
-            // queryArr = query.split(/\s/);
-            // var emphasized = sample;
-            // for (var i=0; i < queryArr.length; i++) {
-            //     emphasized = emphasizeWord(emphasized, queryArr[i]);
-            // }
+
             resultdiv = $('#searchresults');
             var searchitem = 
             `<h3> <a href="${url}"> ${pageTitle} </a></h3>
@@ -113,7 +91,6 @@ function findSubarray(arr, subarr) {
 
 function getSampleAllWords(obj, query) {
     var containingText = $(`:ContainsAny(${query})`, obj).text();
-    console.log(`all: \n${containingText}`);
     var textArr = containingText.split(/\s+/);
     var stemmedTextArr = stemWords(containingText);
     var stemmedQueryArr = stemWordsOnly(query);
@@ -139,7 +116,7 @@ function getSampleAllWords(obj, query) {
         var suffix = textArr
             .slice(wordIndex + numQWords, endSuffix)
             .join(' ');
-        phrase = `${prefix} <b>${words}</b> ${suffix}`;
+        phrase = `${prefix} ${words} ${suffix}`;
         
         sampleSet.add(phrase);
     
@@ -151,7 +128,7 @@ function getSampleAllWords(obj, query) {
 }
 
 function matchesAny(wordArr, array) {
-    for (var j=0; wordArr.length; j++) {
+    for (var j=0; j<wordArr.length; j++) {
         for (var i=0; i<array.length; i++) {
             if (wordArr[j] == array[i]) {
                 return true;
@@ -163,22 +140,19 @@ function matchesAny(wordArr, array) {
 
 function getSampleAnyWords(obj, query) {
     var containingText = $(`:ContainsAny(${query})`, obj).text();
-    console.log(`any: \n${containingText}`);
     var textArr = containingText.split(/\s+/);
     var stemmedTextArr = textArr
         .map(x => stemWordsOnly(x));
     var stemmedQueryArr = stemWordsOnly(query);
-
-    var sampleArr = [];
+    var sampleSet = new Set();
+    var mostRecentSample = null;
     while (textArr.length > 0) {
         var i = 0;
         while (i < stemmedTextArr.length && !matchesAny(stemmedTextArr[i], stemmedQueryArr)) {
             i++;
         }
-        console.log(`i: ${i} len: ${textArr.length}`);
         var firstIndex = i;
         var wordGap = 0;
-        break;
         while (i < stemmedTextArr.length && wordGap < WORDS_AROUND) {
             if (!matchesAny(stemmedTextArr[i], stemmedQueryArr)) {
                 wordGap++;
@@ -190,108 +164,50 @@ function getSampleAnyWords(obj, query) {
             i++;
         }
         var lastIndex = i;
-        if (sampleArr.length > 0 && firstIndex < WORDS_AROUND) {
-            var phrase = textArr.slice(firstIndex, lastIndex).join(' ');
-            sampleArr[sampleArr.length-1] += " " + phrase;
+        if (sampleSet.size > 0 && firstIndex < WORDS_AROUND) {
+            var phrase = textArr.slice(0, lastIndex).join(' ');
+            if (mostRecentSample) {
+                mostRecentSample += " " + phrase;
+            }
+            else {
+                mostRecentSample = phrase;
+            }
         }
         else {
-            if (sampleArr.length == MAX_NUM_SAMPLES) {
+            if (sampleSet.size == MAX_NUM_SAMPLES) {
                 break;
             }
             firstIndex = Math.max(0, firstIndex - WORDS_AROUND)
+            lastIndex = Math.min(lastIndex + WORDS_AROUND, textArr.length);
             var phrase = textArr.slice(firstIndex, lastIndex).join(' ');
-            sampleArr.push(emphasizeWords(phrase, query));
+            if (mostRecentSample) {
+                sampleSet.add(mostRecentSample);
+            }
+            mostRecentSample = phrase;
         }
         textArr = textArr.slice(lastIndex);
         stemmedTextArr = stemmedTextArr.slice(lastIndex);
     }
-    return sampleArr;
+    if (mostRecentSample) {
+        sampleSet.add(mostRecentSample);
+    }
+    return Array.from(sampleSet);
 }
 
 function getFullSample(obj, query) {
-    var sections = $('.without-header-inside', obj);
-    console.log(`num sections: ${sections.length}`)
     var sampleArr = [];
-    var i = 0;
-    while (i < sections.length && sampleArr.length < MAX_NUM_SAMPLES) {
-        samples = getSampleAllWords(sections[i], query);
-        console.log(`samples: \n${samples}`);
-        i++;
-    }
-    i = 0;
-    while (i < sections.length && sampleArr.length < MAX_NUM_SAMPLES) {
-        sampleArr = sampleArr.concat(getSampleAnyWords(sections[i], query));
-        i++;
-    }
+    //sampleArr = sampleArr.concat(getSampleAllWords(obj, query));
     if (sampleArr.length < MAX_NUM_SAMPLES) {
-        sampleArr = sampleArr.concat(getSampleAllWords(obj, query));
-    }
-     if (sampleArr.length < MAX_NUM_SAMPLES) {
         sampleArr = sampleArr.concat(getSampleAnyWords(obj, query));    
     }
     if (sampleArr.length > MAX_NUM_SAMPLES) {
         sampleArr = sampleArr.slice(0,MAX_NUM_SAMPLES);
     }
-    sample = sampleArr.join("... ") + "...";
-    return sample;
-}
-
-// fact that most text seems to be in "without-header-inside" divs.
-// If nothing is found in those divs, we look at the entire page.
-function getSample(obj, query, selector) {
-    var sections = $('.without-header-inside', obj);
-    var containsWord = $(`:${selector}("${query}")`, sections);
-    var sample = "";
-    for (var i=0; i<containsWord.length; i++) {
-        var containingText = $(containsWord[i])
-           .text();        
-        sample += containingText + "... ";
-        if (sample.length >= MAX_SAMPLE_LEN) {
-            break;
-        }
-    }
-    sample = sample.substr(0, sample.length-3);
-    if (sample == "") {
-        sample = getSampleFromObj(obj, query, selector);
-    }
-    if (sample.length > MAX_SAMPLE_LEN) {
-        sample = sample.substr(0, MAX_SAMPLE_LEN);
-        sample = sample.substr(0, 
-            Math.min(sample.length, sample.lastIndexOf(" ")));
-    }
-    if (sample.substr(-1) != '.') {
-        sample += "...";
-    }
-    return sample;
-}
-
-// Look for sample text for query in entire page.
-function getSampleFromObj(obj, query, selector) {
-    sample = "";
-    containingText = $(`:${selector}("${query}")`, obj).text();
-    var lowerArr = containingText.toLowerCase().split(' ');
-    var lowerQuery = query.toLowerCase();
-    var index = null;
-    for (var i=0; i<lowerArr.length; i++) {
-        if (lowerArr[i].includes(lowerQuery)) {
-            index = i; 
-            break;
-        }
-    }
-    if (!index) {
-        console.log("query not found");
-        return "..."
-    }
-    var MAX_NUM_WORDS = 60;
-    var first_index = Math.max(index-5, 0);
-    var textArr = containingText.split(" ");
-    var text = textArr
-        .slice(first_index, textArr.length)
-        .join(" ")
-        .replace(/[✎🔗]+/g, '...')
-        .replace(/prevnext|Because of mathjax bug/g," ");
-
-    sample += text + "...";
+    sample = sampleArr
+        .join("... ") + "...";
+    sample = sample.replace(/prevnext|Because of mathjax bug/g,'')
+        .replace(/[✎🔗\s]+/g,' ');
+    sample = emphasizeWords(sample, query);
     return sample;
 }
 
@@ -308,34 +224,6 @@ function boldenInsideLetters(string) {
     return `${split[0]}<b>${match}</b>${split[1]}`;
 }
 
-// Make occurrences of words related to the query through stemming bold.
-function emphasizeWord(string, query) {
-    var queryStemmed = stemmer(query.toLowerCase());
-    var stringArrStemmed = string
-        .toLowerCase()
-        .split(/\s/) 
-        .map(x => x.split(/[^\w]/)
-            .map(y => stemmer(y)));
-    var stringArr = string.split(/\s/);
-
-    var emphArr = []
-
-    if (stringArr.length != stringArrStemmed.length) {
-        console.log("Arrays have different length. Not emphasizing.");
-        return string;
-    }
-    
-    for (var i=0; i<stringArr.length; i++) {
-        if (stringArrStemmed[i].includes(queryStemmed)) {
-            emphArr.push(boldenInsideLetters(stringArr[i]));
-        }
-        else {
-            emphArr.push(stringArr[i]);
-        }
-    }
-    
-    return emphArr.join(" ");
-}
 
 // Make occurrences of words related to the query through stemming bold.
 function emphasizeWords(string, query) {
